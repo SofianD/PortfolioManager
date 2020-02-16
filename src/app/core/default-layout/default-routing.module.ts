9a55f3d66f31ba57441224a86374426d4ca48120
SofianD <doual.sofian@hotmail.com>
2020-02-16 23:51:09 +0100

    Add default routing

---
+++ src/app/core/default-layout/default-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultLayoutComponent } from './default-layout.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: DefaultLayoutComponent,
+    children : []
+  }
+];
+
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class DefaultRoutingModule { }
