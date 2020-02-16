8616f1d91bbefab77032a3a3f21fc9031c0e69bc
SofianD <doual.sofian@hotmail.com>
2020-02-16 23:46:11 +0100

    Add urls in folder environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'http://localhost:3030/api',
+  projectImgUrl: 'http://localhost:3030/images/project',
+  skillImgUrl: 'http://localhost:3030/images/project'
 };
 
 /*
