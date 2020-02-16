f88cd9f8536d9bbd62d5e61d7379922cccd22a57
SofianD <doual.sofian@hotmail.com>
2020-02-16 23:50:49 +0100

    Add default module

---
+++ src/app/core/default-layout/default.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HeaderComponent } from '../navigation/header/header.component';
+import { DefaultLayoutComponent } from './default-layout.component';
+import { DefaultRoutingModule } from './default-routing.module';
+
+
+
+@NgModule({
+  declarations: [
+    DefaultLayoutComponent,
+    DefaultRoutingModule,
+    HeaderComponent,
+  ],
+  imports: [
+    CommonModule,
+    DefaultRoutingModule
+  ]
+})
+export class DefaultModule { }
