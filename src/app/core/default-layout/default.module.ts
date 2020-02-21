e8573feb54ca086a233d482fcf022258df6af2e4
SofianD <doual.sofian@hotmail.com>
2020-02-21 17:01:44 +0100

    Add Dashboard component in default.module

--- src/app/core/default-layout/default.module.ts
+++ src/app/core/default-layout/default.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '../navigation/header/header.component';
 import { DefaultLayoutComponent } from './default-layout.component';
 import { DefaultRoutingModule } from './default-routing.module';
-
+import { DashboardComponent } from '../page/dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [
     DefaultLayoutComponent,
     HeaderComponent,
+    DashboardComponent
   ],
   imports: [
     CommonModule,
