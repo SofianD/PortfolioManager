531256a92ecf3dacdad54a9f3dbdf6cdc93c89e1
SofianD <doual.sofian@hotmail.com>
2020-02-21 17:02:02 +0100

    Add the dashboard's route

--- src/app/core/default-layout/default-routing.module.ts
+++ src/app/core/default-layout/default-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultLayoutComponent } from './default-layout.component';
 import { AuthGuard } from 'src/app/shared/guards/auth.guard';
+import { DashboardComponent } from '../page/dashboard/dashboard.component';
 
 
 const routes: Routes = [
@@ -9,7 +10,17 @@ const routes: Routes = [
     path: '',
     component: DefaultLayoutComponent,
     canActivateChild: [AuthGuard],
-    children : []
+    children : [
+      {
+        path: '',
+        redirectTo: 'dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
   }
 ];
 
