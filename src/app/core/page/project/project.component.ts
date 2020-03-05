dd12de876a4323bff718bcfde396cc9e23a248ae
SofianD <doual.sofian@hotmail.com>
2020-03-05 18:14:53 +0100

    #72 import Project service in the project controller

--- src/app/core/page/project/project.component.ts
+++ src/app/core/page/project/project.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProjectService } from 'src/app/shared/services/project/project.service';
 
 @Component({
   selector: 'app-project',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProjectComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
   }
