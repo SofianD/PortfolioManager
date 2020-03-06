da6068fa6f74822931a8c78b8f2b0991b3f02bfc
SofianD <doual.sofian@hotmail.com>
2020-03-06 11:18:23 +0100

    #72 create crude functions in project service

--- src/app/shared/services/project/project.service.ts
+++ src/app/shared/services/project/project.service.ts
@@ -1,9 +1,44 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment as env } from '../../../../environments/environment';
+const BACKEND_URL = env.apiUrl;
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProjectService {
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  async getAllProjects(): Promise<any> {
+    return this.http.get(BACKEND_URL + 'project');
+  }
+
+  async getOneProject(id: string): Promise<any> {
+    return this.http.get(BACKEND_URL + 'project/' + id);
+  }
+
+  async create(project: any): Promise<any> {
+    return this.http.post(
+      BACKEND_URL + 'project',
+      {
+        project
+      }
+    );
+  }
+
+  async update(id: string, project: any): Promise<any> {
+    return this.http.put(
+      BACKEND_URL + 'project/' + id,
+      {
+        project
+      }
+    );
+  }
+
+  async delete(id: string): Promise<any> {
+    return this.http.delete(BACKEND_URL + 'project/' + id);
+  }
 }
