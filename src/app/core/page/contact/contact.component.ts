edb0df12e81c168acbb284a1ba7f1d9e6d124014
SofianD <doual.sofian@hotmail.com>
2020-03-07 21:16:56 +0100

    #74 setup contact controller

--- src/app/core/page/contact/contact.component.ts
+++ src/app/core/page/contact/contact.component.ts
@@ -8,11 +8,47 @@ import { ContactService } from 'src/app/shared/services/contact/contact.service'
 })
 export class ContactComponent implements OnInit {
 
+  messages: [] = [];
+
+  selectedMessage: object = {};
+
   constructor(
     private contactService: ContactService
-  ) { }
+  ) {}
 
   ngOnInit() {
   }
 
+  /////////////////////////////////////////////////
+  ////    SERVICE FUNCTIONS
+
+  async getMessages() {
+    try {
+      (await this.contactService.getMessages()).subscribe(res => {
+        this.messages = res.messages;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async getOneMessage(id: string) {
+    try {
+      (await this.contactService.getOneMessage(id)).subscribe(res => {
+        this.selectedMessage = res.result;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async delete(id: string) {
+    try {
+      (await this.contactService.delete(id)).subscribe(res => {
+        this.selectedMessage = res.result;
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
