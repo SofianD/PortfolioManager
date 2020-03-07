4421f4972d90fa4d53d116987aef51e900ebce1f
SofianD <doual.sofian@hotmail.com>
2020-03-07 19:51:38 +0100

    #72 fixed view

--- src/app/core/page/skill/skill.component.ts
+++ src/app/core/page/skill/skill.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SkillComponent implements OnInit {
 
+  mode: string = 'overview';
+
+  skills: [] =[];
+
   constructor() { }
 
   ngOnInit() {
