467797b1662e7199c7dbbb6bb5a154f660351784
SofianD <doual.sofian@hotmail.com>
2020-03-07 18:13:42 +0100

    #72 create image functions

--- src/app/core/page/project/project.component.ts
+++ src/app/core/page/project/project.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ProjectService } from 'src/app/shared/services/project/project.service';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-project',
@@ -11,15 +11,17 @@ export class ProjectComponent implements OnInit {
 
 
   dateNow = Date.now();
+  projects: [] = [];
 
   // define the view
   mode = 'overview';
 
-  // declare form of type FormGroup
   form: FormGroup;
   submitted: boolean = false;
 
-  projects: [] = [];
+  //image preview
+  imagePicked: string = '';
+  previewIsVisible: boolean = false;
 
   constructor(
     private projectService: ProjectService,
@@ -84,7 +86,6 @@ export class ProjectComponent implements OnInit {
   submitForm() {
     this.submitted = true;
     if (this.form.invalid) {
-      console.log('error');
       console.log(Object.entries(this.form.value));
       return;
     }
@@ -100,8 +101,35 @@ export class ProjectComponent implements OnInit {
   /////////////////////////////////////////////////////////////////////
   ////  IMAGE FUNCTIONS
   onImagePicked(event: Event) {
-
+    const file = (event.target as HTMLInputElement).files[0];
+    !this.checkFileType(file.name.split('.')[file.name.split('.').length - 1])
+    ?
+    console.log('Wrong') :
+    this.setupImgPreview(file);
   }
+
+  checkFileType(type: string) {
+    const validTypes = ['jpg', 'jpeg', 'png'];
+    return validTypes.includes(type.toLowerCase()) ? true : false;
+  }
+
+  addImageInForm(image: string, title: string, description: string) {
+    this.formImages.push(this.createItem({
+      image,
+      title,
+      description
+    }));
+  }
+
+  setupImgPreview(file: File) {
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      this.imagePicked = reader.result.toString();
+    };
+    reader.readAsDataURL(file);
+    this.previewIsVisible = true;
+  }
+
 
   /////////////////////////////////////////////////////////////////////
   ////  VIEW FUNCTIONS
@@ -140,4 +168,12 @@ export class ProjectComponent implements OnInit {
   async delete(id: string) {
     const response = await this.projectService.delete(id);
   }
+
+
+  //////////////////////////////
+  //// Getters:
+  get formImages(): FormArray {
+    return this.form.get('images') as FormArray;
+  }
+
 }
