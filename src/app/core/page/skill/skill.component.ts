6ee4fb32f1739b6f4b142a822f2dbb6308dfc94d
SofianD <doual.sofian@hotmail.com>
2020-03-07 20:31:53 +0100

    # add feat to use forms

--- src/app/core/page/skill/skill.component.ts
+++ src/app/core/page/skill/skill.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { SkillService } from 'src/app/shared/services/skill/skill.service';
 
 @Component({
   selector: 'app-skill',
@@ -9,11 +11,107 @@ export class SkillComponent implements OnInit {
 
   mode: string = 'overview';
 
-  skills: [] =[];
+  //Platform section
+  platform: [] =[];
+  platformForm: FormGroup;
 
-  constructor() { }
+  //Framework section
+  frameworks: [] =[];
+  frameworkForm: FormGroup;
+
+  //Skill section
+  skills: [] =[];
+  skillForm: FormGroup;
+
+  constructor(
+    private skillService: SkillService,
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit() {
+    this.newSkillForm();
+    this.newFmForm();
+    this.newPlatformForm();
+  }
+
+  changePageMode(string: string) {
+    this.mode = string;
+  }
+
+  //////////////////////////////////////////////////////////////
+  ////   SKILL SECTION
+  newSkillForm() {
+    this.skillForm = this.fb.group({
+      name: new FormControl(
+        null, {
+          validators: [
+            Validators.required
+          ]
+        }
+      )
+    })
+  }
+
+  submitSkillForm() {
+    if (this.skillForm.invalid) {
+      console.log('error');
+      return;
+    }
+  }
+
+  resetSkillForm() {
+    this.skillForm.reset()
+  }
+
+  //////////////////////////////////////////////////////////////
+  ////   FRAMEWORK SECTION
+  newFmForm() {
+    this.frameworkForm = this.fb.group({
+      name: new FormControl(
+        null, {
+          validators: [
+            Validators.required
+          ]
+        }
+      )
+    })
+  }
+
+  submitFmForm() {
+    if (this.frameworkForm.invalid) {
+      console.log('error');
+      return;
+    }
+  }
+
+  resetFmForm() {
+    this.frameworkForm.reset()
+  }
+
+
+  //////////////////////////////////////////////////////////////
+  ////   PLATFORM SECTION
+  newPlatformForm() {
+    this.platformForm = this.fb.group({
+      name: new FormControl(
+        null, {
+          validators: [
+            Validators.required
+          ]
+        }
+      )
+    })
+  }
+
+  submitPlatformForm() {
+    if (this.platformForm.invalid) {
+      console.log('error');
+      return;
+    }
+  }
+
+  resetPlatformForm() {
+    this.platformForm.reset()
   }
 
 }
